d7f76cda270234de4a6ccce2e1c116aa0cda2475
Vahid Vakili <v.vakili73@gmail.com>
2019-01-09 13:31:39 +0330

    Commit...

--- __main__.py
+++ __main__.py
@@ -21,6 +21,7 @@ from tensorflow.keras import backend as K
 if __name__ == "__main__":
 
     np.random.seed(0)
+    tf.set_random_seed(0)
 
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
